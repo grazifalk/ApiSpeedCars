4398ee21a1a1e42b4c2029f100e44be38e8c47fd
Grazi Falk <83370784+grazifalk@users.noreply.github.com>
2023-09-12 17:50:31 -0300

    atualizacoes

--- WebApiSpeedCars/Program.cs
+++ WebApiSpeedCars/Program.cs
@@ -27,7 +27,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("MyCorsPolicy", builder =>
     {
-        builder.WithOrigins("http://localhost:5173")
+        builder.WithOrigins("http://localhost:5174", "http://127.0.0.1:5174")
             .AllowAnyMethod()
             .AllowAnyHeader();
     });
